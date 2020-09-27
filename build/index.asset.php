13ad5c70820bae7bd33f94df3b12cd527cbce9ae
Herb Miller <herb@bobbingwide.com>
2020-09-27 15:37:40 +0100

    Issue #1 - dev build of first prototype

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3d3123b64383d3c4e5fb9b0e91d55dce');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9ba3c9e9c067506d51fe96a1005e016a');
