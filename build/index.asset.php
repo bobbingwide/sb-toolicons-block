69368800a7d31cd2379f8b6f252f0b8a0cd8b63b
Herb Miller <herb@bobbingwide.com>
2020-09-28 15:46:02 +0100

    Issue #1 - commit latest build version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9ba3c9e9c067506d51fe96a1005e016a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'e02e86a860ae1bf3fc97a12cda71f7a5');
