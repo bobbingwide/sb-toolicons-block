d980e882852f52044d2ab7f402f697007277a903
Herb Miller <herb@bobbingwide.com>
2020-09-29 14:13:49 +0100

    Issue #1, issue #2 - commit latest build version and language files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'ff86273e4b5f3c9f25be30571647681d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'fdd41f2726d66f5a501f3182effbe8eb');
